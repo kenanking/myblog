58ff37435e0fdf7a1e169236c40a9871d11a5f84
kenanking <360383464@qq.com>
2019-08-17 14:04:34 +0800

    third commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from datetime import datetime
 from .models import Post
@@ -17,3 +17,11 @@ def homepage(request):
     posts = Post.objects.all()
     now = datetime.now()
     return render(request, 'index.html', locals())
+
+def showpost(request, slug):
+    try:
+        post = Post.objects.get(slug = slug)
+        if post != None:
+            return render(request, 'post.html', locals())
+    except:
+        return redirect('/')
