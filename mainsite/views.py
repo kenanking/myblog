c2e5e0d2f2f3b2db10fd7a0b135ff8ee5c23b2da
kenanking <360383464@qq.com>
2019-08-17 13:23:36 +0800

    sencond commit

--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+from .models import Post
 
 # Create your views here.
+# def homepage(request):
+#     posts = Post.objects.all()
+#     post_lists = list()
+#     for count, post in enumerate(posts):
+#         post_lists.append("No.{}:".format(str(count)) + str(post) + "<hr>")
+#         post_lists.append("<small>" + str(post.body.encode('utf-8'))+"</small><br><br>")
+
+#     return HttpResponse(post_lists)
+
+def homepage(request):
+    posts = Post.objects.all()
+    now = datetime.now()
+    return render(request, 'index.html', locals())
--- myblog/settings.py
+++ myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*p1+^7bp_b8og(yt!yy%r&lvdb@2lx6b=u2uo)q#ag#y*2r8@@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', ]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainsite',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'myblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
